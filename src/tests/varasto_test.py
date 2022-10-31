eb53803823e5d33f09ee8132735b378b3836d94a
Simo Soini <caps@foobar.fi>
2022-10-31 16:57:49 +0200

    tests: Updated test coverage of varasto.py to 100%.

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,38 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_konstruktori_luo_negatiivisen_varaston(self):
+        varasto = Varasto(-1)
+        self.assertAlmostEqual(varasto.tilavuus, 0)
+
+    def test_uudella_varastolla_negatiivinen_alkusaldo(self):
+        varasto = Varasto(10, -1)
+        self.assertAlmostEqual(varasto.saldo, 0)
+
+    def test_uusi_varasto_suurempi_saldo_kuin_tilavuus(self):
+        varasto = Varasto(1, 10)
+        self.assertAlmostEqual(varasto.saldo, 1)
+
+    def test_lisaa_negatiivinen_maara(self):
+        saldo = self.varasto.saldo
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, saldo)
+
+    def test_lisaa_enemman_kuin_mahtuu(self):
+        self.varasto.lisaa_varastoon(12)
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_ota_varastosta_negatiivinen_maara(self):
+        self.varasto.lisaa_varastoon(5)
+        maara = self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(maara, 0)
+
+    def test_ota_varastosta_liikaa(self):
+        self.varasto.lisaa_varastoon(5)
+        maara = self.varasto.ota_varastosta(10)
+        self.assertAlmostEqual(maara, 5)
+
+    def test_merkkijonoesitys(self):
+        mjono = self.varasto.__str__()
+        self.assertAlmostEqual(mjono, 'saldo = 0, vielä tilaa 10')
