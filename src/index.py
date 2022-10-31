efce0d664b247619386062a323be3dae7637b809
Simo Soini <simo@capscience.fi>
2022-10-31 18:51:16 +0200

    index: removed hello world print

--- src/index.py
+++ src/index.py
@@ -53,7 +53,6 @@ def main():
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
-    print("Hello, world!")
 
 
 if __name__ == "__main__":
