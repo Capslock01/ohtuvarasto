659ed29f9b66de8f746e4ca0b4761652e223cea7
Simo Soini <simo@capscience.fi>
2022-11-10 21:29:20 +0200

    Broke specified pylint rules for ex. 3

--- src/varasto.py
+++ src/varasto.py
@@ -15,9 +15,16 @@ class Varasto:
         else:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
+            val = True
+            if val:
+                pass
+            else:
+                if val:
+                    pass
+                else:
+                    pass
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää
-    # viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
